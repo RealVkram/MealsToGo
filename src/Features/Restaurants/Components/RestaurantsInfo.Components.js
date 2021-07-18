159c893ca20060906cef2734627fc223db6ee51a
RealVkram <ajayiadegbemi.com>
2021-07-18 16:05:26 +0100

    refactoring to styled componenets

--- src/Features/Restaurants/Components/RestaurantsInfo.Components.js
+++ src/Features/Restaurants/Components/RestaurantsInfo.Components.js
@@ -9,27 +9,42 @@ import {
   RefreshControl,
 } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components/native";
 
 export const RestaurantsInfo = ({ restaurant = {} }) => {
   const {
     name = "Nkwobi",
     icon,
-    photos = ["https://picsum.photos/700", "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"],
+    photos = [
+      "https://picsum.photos/700",
+      "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    ],
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
 
   return (
-    <Card style={styles.card} elevation={5}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[1]}}/>
-      <Text style={styles.text}>{name}</Text>
-    </Card>
+    <RestCard elevation={5}>
+      <Cover key={name} source={{ uri: photos[1] }} />
+      <Title>{name.toUpperCase()}</Title>
+    </RestCard>
   );
 };
 
-const styles = StyleSheet.create({
-  card: { backgroundColor: "purple" },
-  cover: { padding: 20, backgroundColor: "green" },
-  text: { color: "white", padding: 15, fontWeight: 50 },
-});
+const Title = styled.Text`
+  font-size: 1.2em;
+  font-weight: bold;
+  color: #fff;
+  padding: 0.5em;
+  align-self: center;
+  justify: center;
+`;
+const Cover = styled(Card.Cover)`
+  background-color: green;
+  padding: 0.5em;
+`;
+
+const RestCard = styled(Card)`
+  background-color: purple;
+`;
